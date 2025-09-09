ee2850214c01ca4920a8722d4ba261030e778bfd
MOHAMED AIT TAJANTE <mhdjant607@gmail.com>
2025-09-09 20:56:43 +0100

    ex01 prime check

--- ex01/Program.java
+++ ex01/Program.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Program
 {
     public static void  main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-
+        int number = input.nextInt();
+        if (number <= 1)
+        {
+            System.err.println("IllegalArgument");
+            System.exit(-1);
+        }
+        int counter = 0;
+        boolean isPrime = false;
+        while ()
+        System.out.println("--> " + number);
     }
 }
